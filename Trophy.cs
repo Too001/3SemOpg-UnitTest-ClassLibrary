1cfe62378c409907c9b0113afd4440e53df9bf7c
ozdemir02 <wareznipe@gmail.com>
2024-09-18 10:16:59 +0200

    Add project files.

---
+++ Trophy.cs
@@ -0,0 +1,14 @@
+﻿namespace _3SemObligatoriskOpgave_1_og_2
+{
+    public class Trophy
+    {
+        public int Id { get; set; }
+        public string Competetion { get; set; }
+        public string Year { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
